1c5c1f023e7a43ce8b63fa1d82348f7b10e4d4a7
arvmrt <46164832+arvmrt@users.noreply.github.com>
2024-03-04 16:40:46 -0600

    Update openai_gpt_stream_output.py

--- openai_gpt_stream_output.py
+++ openai_gpt_stream_output.py
@@ -11,7 +11,7 @@ prompt = "Who is the first president of USA?."
 
 # Initilize model and it will automatically pass API key via environment variable OPENAI_API_KEY
 # Initialize OpenAI Chat Model
-chat = ChatOpenAI()
+client = OpenAI()
 
 # Send prompt question and get response from Model
 chat_completion = client.chat.completions.create(
